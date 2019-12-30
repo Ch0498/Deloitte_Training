be07b1e8681627c6637e635674c0583321f24ed2
Ch0498 <59251553+Ch0498@users.noreply.github.com>
2019-12-30 17:58:44 +0530

    Add files via upload

---
+++ MyCalender.java
@@ -0,0 +1,29 @@
+package day6;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class MyCalender {
+
+	
+	public static void main(String[] args) {
+		
+		Calendar cal = Calendar.getInstance();
+		System.out.println(cal);
+		int yr = cal.get(Calendar.YEAR);
+		int mon = cal.get(Calendar.MONTH);
+		int day = cal.get(Calendar.DATE);
+		
+		System.out.println(day+" : "+(mon+1)+" : "+yr);
+		
+		Date dt = new Date();
+		long millis = dt.getTime();
+		Calendar calndr = Calendar.getInstance();
+		calndr.setTime(dt);
+		calndr.setTimeInMillis(millis);
+		
+		
+	}
+	
+	
+}
